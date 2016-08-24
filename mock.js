8ef648f7c39225c861793cb2d1d123aafced4647
Kent Merrell <kmerrell@gmail.com>
2016-08-24 12:54:47 -0400

    generated mock.js with scaffold.

--- mock.js
+++ mock.js
@@ -14,6 +14,86 @@ var createMock = function (options) {
     cors: true
   });
   mock.jsonRoute({
+    name: "POST-localhost/summaryLevelService",
+    method: "POST",
+    path: "/summaryLevelService",
+    hostname: "localhost",
+    handlers: {
+      "0-POST-localhost/summaryLevelService": {
+        description: "0-POST-localhost/summaryLevelService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"facets\\\":[\\\"summaryLevels\\\"],\\\"geoIds\\\":[],\\\"topics\\\":[],\\\"naicsCodes\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/0-POST-localhost%2FsummaryLevelService.json");
+        }
+      },
+      "1-POST-localhost/summaryLevelService": {
+        description: "1-POST-localhost/summaryLevelService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"facets\\\":[\\\"summaryLevels\\\"],\\\"geoIds\\\":[\\\"0100000US\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/1-POST-localhost%2FsummaryLevelService.json");
+        }
+      },
+      "2-POST-localhost/summaryLevelService": {
+        description: "2-POST-localhost/summaryLevelService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"facets\\\":[\\\"summaryLevels\\\"],\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/2-POST-localhost%2FsummaryLevelService.json");
+        }
+      },
+      "3-POST-localhost/summaryLevelService": {
+        description: "3-POST-localhost/summaryLevelService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"facets\\\":[\\\"summaryLevels\\\"],\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\",\\\"1600000US4967000\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/3-POST-localhost%2FsummaryLevelService.json");
+        }
+      }
+    }
+  });
+  mock.jsonRoute({
+    name: "POST-localhost/facetService",
+    method: "POST",
+    path: "/facetService",
+    hostname: "localhost",
+    handlers: {
+      "0-POST-localhost/facetService": {
+        description: "0-POST-localhost/facetService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"geoIds\\\":[\\\"0100000US\\\"],\\\"topics\\\":[],\\\"naicsCodes\\\":[],\\\"facets\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/0-POST-localhost%2FfacetService.json");
+        }
+      },
+      "1-POST-localhost/facetService": {
+        description: "1-POST-localhost/facetService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"geoIds\\\":[\\\"0100000US\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[],\\\"facets\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/1-POST-localhost%2FfacetService.json");
+        }
+      },
+      "2-POST-localhost/facetService": {
+        description: "2-POST-localhost/facetService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[],\\\"facets\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/2-POST-localhost%2FfacetService.json");
+        }
+      },
+      "3-POST-localhost/facetService": {
+        description: "3-POST-localhost/facetService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\",\\\"1600000US4967000\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[],\\\"facets\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/3-POST-localhost%2FfacetService.json");
+        }
+      }
+    }
+  });
+  mock.jsonRoute({
     name: "POST-localhost/typeAheadService",
     method: "POST",
     path: "/typeAheadService",
@@ -21,20 +101,42 @@ var createMock = function (options) {
     handlers: {
       "0-POST-localhost/typeAheadService": {
         description: "0-POST-localhost/typeAheadService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"text\\\":\\\"davis\\\",\\\"type\\\":\\\"geo\\\",\\\"geoIds\\\":[\\\"0100000US\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
         handler: function (request) {
           return require("./fixtures/0-POST-localhost%2FtypeAheadService.json");
         }
       },
       "1-POST-localhost/typeAheadService": {
         description: "1-POST-localhost/typeAheadService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"text\\\":\\\"salt\\\",\\\"type\\\":\\\"geo\\\",\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
         handler: function (request) {
           return require("./fixtures/1-POST-localhost%2FtypeAheadService.json");
         }
       },
       "2-POST-localhost/typeAheadService": {
         description: "2-POST-localhost/typeAheadService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"text\\\":\\\"salt lake\\\",\\\"type\\\":\\\"geo\\\",\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
         handler: function (request) {
           return require("./fixtures/2-POST-localhost%2FtypeAheadService.json");
+        }
+      }
+    }
+  });
+  mock.jsonRoute({
+    name: "POST-localhost/tableService",
+    method: "POST",
+    path: "/tableService",
+    hostname: "localhost",
+    handlers: {
+      "0-POST-localhost/tableService": {
+        description: "0-POST-localhost/tableService",
+        requestQueryString: "[]",
+        requestPostData: "{\"mimeType\":\"application/json;charset=UTF-8\",\"text\":\"{\\\"request\\\":{\\\"geoIds\\\":[\\\"0100000US\\\",\\\"0500000US49011\\\",\\\"1600000US4967000\\\"],\\\"topics\\\":[\\\"Age\\\"],\\\"naicsCodes\\\":[]}}\"}",
+        handler: function (request) {
+          return require("./fixtures/0-POST-localhost%2FtableService.json");
         }
       }
     }
